fd9bd3521713bc15c68a0adacd57ea95a88f4deb
ChristFarr <christianofarrellrachman@gmail.com>
2025-09-26 18:03:21 +0800

    commit

--- Homework/Homework 01 190925/Calculus.py
+++ Homework/Homework 01 190925/Calculus.py
@@ -13,7 +13,7 @@ def integral(f, a, b):
 
 def theorem1(f, x):
     r = df(lambda x:integral(f, 0, x), x)
-    print('r=', r, 'f(x)=', f(x))
+    print('r =', r, 'f(x) =', f(x))
     print('abs(r-f(x))<0.01 = ', abs(r-f(x))<0.01)
     assert abs(r-f(x))<0.01
 
